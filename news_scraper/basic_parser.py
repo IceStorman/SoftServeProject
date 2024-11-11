9620f121765e7e5f0e9002a6307a5cb0e8804dbd
Taras Protsiv <protsiv.taras2006@gmail.com>
2024-11-11 19:34:40 +0200

    start

---
+++ news_scraper/basic_parser.py
@@ -0,0 +1,6 @@
+class sport_parser:
+    def __init__(self, url):
+        self.url = url
+
+    def fetch_page(self, url_suffix=''):
+        pass
---
+++ news_scraper/news_scraper.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import json
+sport_urls = {
+   ' main_football_url' : 'https://www.espn.com/soccer/',
+   ' main_afl_url' : '',
+   ' main_baseball_url' : 'https://www.espn.com/mlb/',
+    'main_basketball_url' : '',
+    'main_formula_1_url' : 'https://www.espn.com/f1/',
+    'main_handball_url' : '',
+    'main_hockey_url' : 'https://www.espn.com/nhl/',
+    'main_mma_url' : 'https://www.espn.com/mma/',
+    'main_nba_url' : 'https://www.espn.com/nba/',
+    'main_nfl_url' : 'https://www.espn.com/nfl/',
+    'main_rugby_url' : 'https://www.espn.com/rugby/',
+    'main_volleyball_url' : ''
+}
+
+
+
+print(sport_urls[' main_football_url'])
+#for key, value in d.items():
