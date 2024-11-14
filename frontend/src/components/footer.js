9b3726427aec2849bb5a55f8520cba2fd982c22e
Andrew Kozovyi <andriy.kozovyi@gmail.com>
2024-11-14 20:42:00 +0200

    SPECIAL COMMIT FOR DIMA | BUTTON FOR DIMA

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,12 +1,26 @@
 import React from "react";
 
+function scrollTop(){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
+
 function footer(){
 
     return (
         <footer>
 
-            <h1>Vlad help: +380 97 584 22 42</h1>
-            <h1>Vlad home: Коломийська 19, кв 20, 5 поверх, 1 під'їзд</h1>
+            <button className={"scrollBtn"} onClick={scrollTop}>
+                <i className="fa fa-solid fa-arrow-up fa-10x"></i>
+            </button>
+
+            <div className={"footer-info"}>
+                <h1>Vlad help: +380 97 584 22 42</h1>
+                <h1>Vlad home: Коломийська 19, кв 20, 5 поверх, 1 під'їзд</h1>
+            </div>
+
 
         </footer>
     );
