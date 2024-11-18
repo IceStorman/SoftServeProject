7c4782c7aa0d11d8995fa8515e043e048cb336e6
torii044 <kovalchuk.v466@gmail.com>
2024-11-18 21:16:35 +0200

    + TeamIndex table in migration

--- database/session.py
+++ database/session.py
@@ -7,4 +7,3 @@ DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres-
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-#цей файл має бути в postgres?
