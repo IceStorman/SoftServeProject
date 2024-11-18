b6e95b7d9834e74af02867c0b2a10c45cf9439ca
Taras4568 <protsiv.taras2006@gmail.com>
2024-11-18 22:38:32 +0200

    scraper+model

--- news_scraper/test.py
+++ news_scraper/test.py
@@ -3,8 +3,7 @@ from unittest.mock import patch, MagicMock
 from bs4 import BeautifulSoup
 import requests
 from functionality import Main_page_sport_parser, Article_Scraper
-
-
+from tokenizator import what_teams_here
 
 main_url = "https://www.espn.com/nba/" 
 parser = Article_Scraper(main_url)
@@ -12,10 +11,25 @@ articles = parser.get_article_links(max_articles=3)
 
 if articles:
     for article in articles:
-        parser.print_article(article)
-        print('\n')
+        parser.print_article(article)  
+
+        
+        content = parser.get_article_content(article['url'])
+        if content:
+            
+            article_text = "\n".join(
+                " ".join(section['content']) for section in content['article'].values()
+            )
+
+            
+            teams = what_teams_here(article_text)
+            print("\nTeams mentioned in the article:")
+            print(teams)
+        else:
+            print("Failed to fetch the content of the article.")
 else:
-    print("No articles found.")  
+    print("No articles found.")
+
 
 
 
