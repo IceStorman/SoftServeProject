5777304a71ac284260020b5e8ee4bad6abbba023
Andrew <andriy.kozovyi@gmail.com>
2024-11-18 15:00:49 +0200

    vlad help number fix

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -17,7 +17,7 @@ function footer(){
             </button>
 
             <div className={"footerInfo"}>
-                <h1>Vlad help: +380 97 584 22 42</h1>
+                <h1>Vlad help: +380 97 584 22 34</h1>
                 <h1>Vlad home: Коломийська 19, кв 20, 5 поверх, 1 під'їзд</h1>
             </div>
 
