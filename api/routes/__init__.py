58764e422eaccdd8f37f21026fcf1c9fe001851d
Iarynovskyi <dimayar20061@gmail.com>
2024-11-22 12:30:41 +0200

    added cache

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,18 +1,20 @@
 from flask import Flask
 from flask_cors import CORS
 from api.routes import api_news, api_games, api_sports
-
+from api.routes.cache import cache
 
 # Ініціалізація додатку
 def create_app():
     app = Flask(__name__)
     CORS(app)
+    app.config['CACHE_TYPE'] = 'SimpleCache'  # Тип кешу (наприклад, Redis)
+    app.config['CACHE_DEFAULT_TIMEOUT'] = 300  # За замовчуванням кеш діє 5 хвилин
+    cache.init_app(app)
     app.register_blueprint(api_news.news_app, url_prefix='/news')
     app.register_blueprint(api_games.games_app, url_prefix='/games')
     app.register_blueprint(api_sports.sports_app, url_prefix='/sports')
 
     return app
-
 
 
 app = create_app()
--- api/routes/api_news.py
+++ api/routes/api_news.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, jsonify
 from service.api_logic.news_logic import get_news_by_count, get_latest_sport_news
 from database.session import SessionLocal
+from api.routes.cache import cache
 
 session = SessionLocal()
 news_app = Blueprint('news', __name__)
 
 @news_app.route('/recent', methods=['GET'])
+@cache.cached(timeout=60*60)
 def get_recent_news_endpoint():
     try:
         recent_news = get_news_by_count(5, session)
@@ -15,6 +17,7 @@ def get_recent_news_endpoint():
 
 
 @news_app.route('/<sport_type>', methods=['GET'])
+@cache.cached(timeout=60*60)
 def get_sport_news_endpoint(sport_type):
     try:
         recent_news = get_latest_sport_news(5, sport_type, session)
