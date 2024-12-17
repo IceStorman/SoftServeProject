3283969e0e4f3d30724f0a577de8b82a17981def
IceStorman <vladgamedeveloper@gmail.com>
2024-12-17 15:35:42 +0200

    Fixes

--- service/implementation/auto_request_api/sport_data_managers/abstract_sport_data_manager.py
+++ service/implementation/auto_request_api/sport_data_managers/abstract_sport_data_manager.py
@@ -21,35 +21,21 @@ class AbstractSportDataManager:
     def __init__(self, new_api_data: Dict[str, str]):
         self.api_data = new_api_data
 
+        #TODO: rewrite from dicts to dtos
         self._team_id = self.api_data.get("team_id")
-        if not self._team_id:
-            print("error: Missing or invalid parameter: 'team_id' required.")
-
         self._player_id = self.api_data.get("player_id")
-        if not self._player_id:
-            print("error: Missing or invalid parameter: 'player_id' required.")
-
         self._league_id = self.api_data.get("league_id")
-        if not self._league_id:
-            print("error: Missing or invalid parameter: 'league_id' required.")
-
         self._game_id = self.api_data.get("game_id")
-        if not self._game_id:
-            print("error: Missing or invalid parameter: 'game_id' required.")
-
         self._race_id = self.api_data.get("race_id")
-        if not self._race_id:
-            print("error: Missing or invalid parameter: 'race_id' required.")
-
 
     def main_request(self, host, url, blob_name) -> Dict[str, str]:
         global current_key_index, account_url
 
-        if token_usage[AbstractSportDataManager._sport_name] >= 99:
+        if token_usage[self._sport_name] >= 99:
             current_key_index = (current_key_index + 1) % len(api_key)
-            token_usage[AbstractSportDataManager._sport_name] = 0
+            token_usage[self._sport_name] = 0
 
-        token_usage[AbstractSportDataManager._sport_name] += 1
+        token_usage[self._sport_name] += 1
         headers = {
             'x-rapidapi-host': host,
             'x-rapidapi-key': api_key[current_key_index]
@@ -58,7 +44,7 @@ class AbstractSportDataManager:
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         json_data = response.json()
-        blob_save_specific_api(AbstractSportDataManager._sport_name, blob_name, json_data)
+        blob_save_specific_api(self._sport_name, blob_name, json_data)
 
         return json_data
 
--- service/implementation/auto_request_api/sport_data_managers/sport_facade_data_manager.py
+++ service/implementation/auto_request_api/sport_data_managers/sport_facade_data_manager.py
@@ -7,6 +7,7 @@ from typing import Dict
 
 from service.implementation.auto_request_api.sport_data_managers.afl_data_manager import AflDataManager
 from service.implementation.auto_request_api.sport_data_managers.baseball_data_manager import BaseballDataManager
+from service.implementation.auto_request_api.sport_data_managers.basketball_data_manager import BasketballDataManager
 from service.implementation.auto_request_api.sport_data_managers.football_data_manager import FootballDataManager
 from service.implementation.auto_request_api.sport_data_managers.formula_one_data_manager import FormulaOneDataManager
 from service.implementation.auto_request_api.sport_data_managers.handball_data_manager import HandballDataManager
@@ -27,7 +28,7 @@ class SportFacadeDataManager:
             case "baseball":
                 return BaseballDataManager(api_data)
             case "basketball":
-                return BaseballDataManager(api_data)
+                return BasketballDataManager(api_data)
             case "handball":
                 return HandballDataManager(api_data)
             case "hockey":
