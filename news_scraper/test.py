6eaeb8bdc2d7217dbb93b5e9739a4ebbcea6fefd
Taras4568 <protsiv.taras2006@gmail.com>
2024-12-17 11:58:27 +0200

    deleted work with buffer on c#

--- news_scraper/test.py
+++ news_scraper/test.py
@@ -1,6 +1,14 @@
 import clr
-clr.AddReference("dlls/Custom_Selenium")
+import os
+import sys
+
+dll_path = os.path.join(os.path.dirname(__file__),"dlls")
+sys.path.append(dll_path)
+clr.AddReference("Custom_Selenium")
+
+
 from Selenium_right_click import Driver
+import pyperclip
 
 driver=Driver()
 
@@ -9,3 +17,5 @@ driver.OpenUrl("https://www.espn.com/fantasy/basketball/insider/story/_/id/42388
 driver.PerformRightClickByXpath("//div[@class='img-wrap']//img[@class='imageLoaded']")
 
 driver.Quit()
+
+print(pyperclip.paste())
