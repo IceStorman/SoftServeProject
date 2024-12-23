144b4540d56877ebff50803796fe5f24e31e8245
Taras4568 <protsiv.taras2006@gmail.com>
2024-12-23 15:58:26 +0200

    created class

--- translations_scraper/functionality.py
+++ translations_scraper/functionality.py
@@ -1,6 +1,5 @@
 from service.api_logic.games_logic import get_games_today
 from database.session import SessionLocal
-import json
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,46 +8,63 @@ from selenium.webdriver.common.by import By
 from selenium_stealth import stealth
 
 
-session = SessionLocal()
-options = Options()
-arguments = [
-    "--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-extensions"
-]
-for arg in arguments:
-    options.add_argument(arg)
+class GameScraper:
+    def __init__(self):
+        self.session = SessionLocal()
+        self.driver = self._initialize_webdriver()
+        self.games = []
+        self.future_games = []
 
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=options)
+    @staticmethod
+    def _initialize_webdriver():
+        options = Options()
+        arguments = [
+            "--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-extensions"
+        ]
+        for arg in arguments:
+            options.add_argument(arg)
 
-stealth(driver,
-languages=["en-US", "en"],
-vendor="Google Inc.",
-platform="Win32",
-webgl_vendor="Intel Inc.",
-renderer="Intel Iris OpenGL Engine",
-fix_hairline=True)
+        service = Service(ChromeDriverManager().install())
+        driver = webdriver.Chrome(service=service, options=options)
 
-def append_future_games():
-    games = get_games_today(session=session)
+        stealth(driver,
+                languages=["en-US", "en"],
+                vendor="Google Inc.",
+                platform="Win32",
+                webgl_vendor="Intel Inc.",
+                renderer="Intel Iris OpenGL Engine",
+                fix_hairline=True)
+        return driver
 
-    future_games = [
-        f"{game['home_team_name']} vs {game['away_team_name']}" 
-        for game in games if game['status'] == 'Not Started'
-    ]
-    return future_games
+    def fetch_games(self):
+        self.games = get_games_today(session=self.session)
 
-def search_for_games(future_games):
+    def filter_future_games(self):
+        self.future_games = [
+            f"{game['home_team_name']} vs {game['away_team_name']} {game['date']}"
+            for game in self.games if game['status'] == 'Not Started'
+        ]
 
-    games = append_future_games()
-
-    for game in future_games:
+    def search_game_links(self):
+        links = []
         base_url = "https://www.google.com/search"
-        search_url = f"{base_url}?q={game.replace(' ', '+')}+watch"
 
+        for game in self.future_games:
+            search_url = f"{base_url}?q={game.replace(' ', '+')}+watch"
+            self.driver.get(search_url)
+            results = self.driver.find_elements(By.CLASS_NAME, 'LC20lb MBeuO DKV0Md')[:3]
+            game_links = [result.text for result in results]
+            links.append({
+                'game': game,
+                'links': game_links
+            })
+        return links
 
-
-
-
-
+    def run(self):
+        self.fetch_games()
+        self.filter_future_games()
+        links = self.search_game_links()
+        print("Future Games:", self.future_games)
+        print("Links:", links)
 
 
--- translations_scraper/test.py
+++ translations_scraper/test.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -20,6 +21,8 @@ try:
 
     
     time.sleep(5)
+
+    driver.find_elemens(By)
 finally:
  
     driver.quit()
