13fd64f55c274e79a882d4df28cb3da743cdba78
Iarynovskyi <dimayar20061@gmail.com>
2025-01-03 11:36:09 +0200

    new tables for db

--- database/models/country.py
+++ database/models/country.py
@@ -4,7 +4,5 @@ class Country(Base):
     __tablename__ = 'Country'
     country_id = Column(Integer, primary_key=True)
     flag = Column(String)
-    sport_id = Column(ForeignKey('Sports.sport_id'))
     name = Column(String)
     code = Column(String)
-    api_id = Column(Integer)
--- database/models/teams_index.py
+++ database/models/teams_index.py
@@ -3,10 +3,8 @@ from .base import Base, Column, Integer, String, ForeignKey
 class TeamIndex(Base):
     __tablename__ = 'TeamIndex'
     team_index_id = Column(Integer, primary_key=True)
-    news_id = Column(ForeignKey('News.news_id'))
     sport_id = Column(ForeignKey('Sports.sport_id'))
     name = Column(String)
     logo = Column(String)
     api_id = Column(Integer)
-    country = Column(ForeignKey('Country.country_id'))
     league = Column(ForeignKey('League.league_id'))
