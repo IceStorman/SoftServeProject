a8b9ebe759449331362478def7ab824762a22ec5
Taras4568 <protsiv.taras2006@gmail.com>
2025-01-07 22:36:26 +0200

    some changes

--- database/models/streams_status.py
+++ database/models/streams_status.py
@@ -4,4 +4,4 @@ class Streams_Status(Base):
     __tablename__ = "Streams_Status"
     stream_id=Column(ForeignKey('Streams.stream_id'), primary_key=True)
     status=Column(String)
-    time=Column(Integer)
+    start_time=Column(Integer)
