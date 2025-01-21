ee1417181487d09d195a11e49985bc76d39303ae
Iarynovskyi <dimayar20061@gmail.com>
2025-01-21 22:10:27 +0200

    new migrations for alembic

--- database/models/streams_status.py
+++ database/models/streams_status.py
@@ -2,5 +2,6 @@ from .base import Base, Column, Integer, String, ForeignKey, Boolean
 
 class Streams_Status(Base):
     __tablename__ = "Streams_Status"
+    streams_status_id = Column(Integer, primary_key=True)
     stream_id=Column(ForeignKey('Streams.stream_id'))
     status_id=Column(ForeignKey('Statuses.status_id'))
