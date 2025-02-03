d5e882f14d337d97098977c7a9031a02598cee01
Taras4568 <protsiv.taras2006@gmail.com>
2025-02-03 12:16:16 +0200

    using container and service at endpoints

--- api/routes/api_news.py
+++ api/routes/api_news.py
@@ -1,9 +1,11 @@
 from flask import Blueprint,request
-from service.api_logic.news_logic import get_news_by_count, get_latest_sport_news, get_popular_news, get_news_by_id
+from service.api_logic.news_logic import NewsService
 from api.routes.cache import cache
 from api.routes.scripts import get_cache_key
 from exept.handle_exeptions import get_custom_error_response, get_exception_error_response
 from exept.exeptions import DatabaseConnectionError, SoftServeException
+from dependency_injector.wiring import inject, Provide
+from api.container.container import Container
 from logger.logger import Logger
 
 logger = Logger("logger", "all.log")
@@ -22,9 +24,9 @@ def handle_db_timeout_error(e):
 @news_app.route('/recent', methods=['GET'])
 @cache.cached(timeout=60*60)
 @logger.log_function_call()
-def get_recent_news_endpoint():
+def get_recent_news_endpoint(service: NewsService = Provide[Container.news_service]):
     try:
-        recent_news = get_news_by_count(COUNT_NEWS)
+        recent_news = service.get_news_by_count(COUNT_NEWS)
         return recent_news
     except SoftServeException as e:
         logger.error(f"Error in GET /: {str(e)}")
@@ -33,9 +35,9 @@ def get_recent_news_endpoint():
 @news_app.route('/<sport_type>', methods=['GET'])
 @cache.cached(timeout=60*60, key_prefix=get_cache_key)
 @logger.log_function_call()
-def get_sport_news_endpoint(sport_type):
+def get_sport_news_endpoint(sport_type, service: NewsService = Provide[Container.news_service]):
     try:
-        sport_news = get_latest_sport_news(COUNT_NEWS, sport_type)
+        sport_news = service.get_latest_sport_news(COUNT_NEWS, sport_type)
         return sport_news
     except SoftServeException as e:
         logger.error(f"Error in GET /: {str(e)}")
@@ -45,9 +47,9 @@ def get_sport_news_endpoint(sport_type):
 @news_app.route('/popular', methods=['GET'])
 @cache.cached(timeout=60*3)
 @logger.log_function_call()
-def get_popular_news_endpoint():
+def get_popular_news_endpoint(service: NewsService = Provide[Container.news_service]):
     try:
-        popular_news = get_popular_news(COUNT_NEWS)
+        popular_news = service.get_popular_news(COUNT_NEWS)
         return popular_news
     except SoftServeException as e:
         logger.error(f"Error in GET /: {str(e)}")
@@ -56,12 +58,12 @@ def get_popular_news_endpoint():
 
 @news_app.route('/article', methods=['POST'])
 @logger.log_function_call()
-def specific_article():
+def specific_article(service: NewsService = Provide[Container.news_service]):
 
     try:
         article = request.get_json()
         news_id=article['blob_id']
-        response = get_news_by_id(news_id)
+        response = service.get_news_by_id(news_id)
         return response
     except SoftServeException as e:
         logger.error(f"Error in POST /: {str(e)}")
