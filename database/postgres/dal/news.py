6f684b4f1a31c045e7022f2c54964a2a2b7a1703
Taras4568 <protsiv.taras2006@gmail.com>
2025-02-03 13:09:33 +0200

    fixing conflict without merge

---
+++ database/postgres/dal/news.py
@@ -0,0 +1,21 @@
+from sqlalchemy import desc
+from database.models import News
+from sqlalchemy.orm import Session
+
+class NewsDAL:
+    def __init__(self, session = None):
+        self.session = session
+
+    def fetch_news(self, order_by=None, limit=None, filters=None):
+        query = self.session.query(News)
+        if filters:
+            query = query.filter(*filters)
+        if order_by:
+            query = query.order_by(order_by)
+        if limit:
+            query = query.limit(limit)
+        return query.all()
+
+    def get_news_by_id(self, blob_id: str):
+        return self.session.query(News).filter(News.blob_id == blob_id).first()
+
--- service/api_logic/news_logic.py
+++ service/api_logic/news_logic.py
@@ -8,65 +8,46 @@ from service.api_logic.scripts import get_sport_by_name
 from database.session import SessionLocal
 from logger.logger import Logger
 
-logger = Logger("logger", "all.log")
 
-session = SessionLocal()
+class NewsService:
+    def __init__(self, news_dal):
+        self._news_dal = news_dal
+        self.logger = Logger("logger", "all.log").logger
+    
 
-@logger.log_function_call()
-def fetch_news(session, order_by: ClauseElement = None, limit: int = None, filters=None):
-    query = session.query(News)
-    if filters:
-        query = query.filter(*filters)
-    if order_by is not None:
-        query = query.order_by(order_by)
-    if limit is not None:
-        query = query.limit(limit)
-    return query.all()
+    def get_news_by_count(self, count: int):
+        news = self._news_dal.fetch_news(order_by=News.save_at.desc(), limit=count)
+        return self.json_news(news)
 
 
-@handle_exceptions
-@logger.log_function_call()
-def get_news_by_count(count: int):
-    news = fetch_news(session, order_by=News.save_at.desc(), limit=count)
-    return json_news(news)
+    def get_latest_sport_news(self, count: int, sport_name: str):
+        sport = get_sport_by_name(self.session, sport_name)
+        filters = [News.sport_id == sport.sport_id]
+        news = self._news_dal.fetch_news(order_by=News.save_at.desc(), limit=count, filters=filters)
+        return self.json_news(news)
 
 
-@handle_exceptions
-@logger.log_function_call()
-def get_latest_sport_news(count: int, sport_name: str):
-    sport = get_sport_by_name(session, sport_name)
-
-    filters = [News.sport_id == sport.sport_id]
-    news = fetch_news(session, order_by=News.save_at.desc(), limit=count, filters=filters)
-    return json_news(news)
+    def get_popular_news(self, count: int):
+        news = self._news_dal.fetch_news(order_by=News.interest_rate.desc(), limit=count)
+        return self.json_news(news)
 
 
-@handle_exceptions
-@logger.log_function_call()
-def get_popular_news(count: int):
-    news = fetch_news(session, order_by=News.interest_rate.desc(), limit=count)
-    return json_news(news)
+    def get_news_by_id(self, blob_id: str):
+        news = self._news_dal.get_news_by_id(blob_id)
+        if news:
+            self.logger.warning(f"News were found: {news}")
+            return self.json_news([news])
 
-@handle_exceptions
-@logger.log_function_call()
-def get_news_by_id(blob_id: str):
-    news = fetch_news(session, filters=[News.blob_id == blob_id], limit=1)
-    if news:
-        logger.warning(f"News ware`t empty: {news}")
-        return json_news(news)
+    def json_news(self, news_records):
+        all_results = []
+        for news_record in news_records:
+            data = blob_get_news(news_record.blob_id)
+            all_results.append({
+                "blob_id": news_record.blob_id,
+                "data": data
+            })
+        return Response(
+            json.dumps(all_results, ensure_ascii=False),
+            content_type='application/json; charset=utf-8',
+        )
 
-
-
-@logger.log_function_call()
-def json_news(news_records):
-    all_results = []
-    for news_record in news_records:
-        data = blob_get_news(news_record.blob_id)
-        all_results.append({
-            "blob_id": news_record.blob_id,
-            "data": data
-        })
-    return Response(
-        json.dumps(all_results, ensure_ascii=False),
-        content_type='application/json; charset=utf-8',
-    )
