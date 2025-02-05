7f02470ec57fc5e2e3497fdf29d9df500ec8f5ae
Taras Protsiv <protsiv.taras2006@gmail.com>
2025-02-05 18:10:34 +0200

    new migration

--- database/models/token_blocklist.py
+++ database/models/token_blocklist.py
@@ -1,14 +1,13 @@
 from .base import Base, Column, Integer, String, ForeignKey, Boolean, DateTime
-import datetime
+import datetime 
 
-class TockenBlocklist(Base):
-    __tablename__ = 'TokenBlocklist'
+class TokenBlocklist(Base):
+    __tablename__ = 'Token_Blocklist'
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("Users.user_id"))
     jti = Column(String(36), nullable=False, index=True)
     token_type = Column(String(10), nullable=False)
     revoked = Column(Boolean, default=False, nullable=False)
     expires_at = Column(DateTime, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)  
-
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)  
