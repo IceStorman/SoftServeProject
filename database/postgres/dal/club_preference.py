c31a86044cd16515e615326745dc622f241f7882
IceStorman <vladgamedeveloper@gmail.com>
2025-02-07 17:55:31 +0200

    Some changes in logic

--- database/postgres/dal/club_preference.py
+++ database/postgres/dal/club_preference.py
@@ -1,14 +1,14 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import text, cast, Unicode
 
-from database.models import ClubPreference
+from database.models import UserClubPreferences
 
-class ClubPreferenceDAL:
+class UserClubPreferenceDAL:
     def __init__(self, db_session: Session):
         self.db_session = db_session
 
-    def get_subscribed_users_on_specific_teams(self, team_name):
-        data = self.db_session.query(ClubPreference).filter(ClubPreference.preferences == team_name).all()
+    def get_subscribed_users_on_specific_teams(self, team_index):
+        data = self.db_session.query(UserClubPreferences).filter(UserClubPreferences.preferences == team_index).all()
 
         user_ids = list()
         for preference in data:
--- service/implementation/email_sender/email_sender.py
+++ service/implementation/email_sender/email_sender.py
@@ -1,26 +1,31 @@
-from database.postgres.dal.club_preference import ClubPreferenceDAL
+from database.postgres.dal.club_preference import UserClubPreferenceDAL
 from database.postgres.dal.userEmail import UserEmailDAL
 from database.session import SessionLocal
 
 session = SessionLocal()
 
-prefs_dal = ClubPreferenceDAL(session)
+prefs_dal = UserClubPreferenceDAL(session)
 user_dal = UserEmailDAL(session)
 
-def try_send_email_to_users(team_name: str):
-    subscribed_users = get_subscribed_users(team_name)
+class EmailSender:
+    @staticmethod
+    def try_send_email_to_users(team_index: int):
+        subscribed_users = EmailSender.get_subscribed_users(team_index)
 
-    for user_id in subscribed_users:
-        user_email = get_user_email(user_id)
-        send_email_to_user(user_email)
+        for user_id in subscribed_users:
+            user_email = EmailSender.get_user_email(user_id)
+            EmailSender.send_email_to_user(user_email)
 
-def get_subscribed_users(team_name: str):
-    return prefs_dal.get_subscribed_users_on_specific_teams(team_name)
+    @staticmethod
+    def get_subscribed_users(team_index: int):
+        return prefs_dal.get_subscribed_users_on_specific_teams(team_index)
 
-def get_user_email(user) -> str:
-    return user_dal.get_user_email(user)
+    @staticmethod
+    def get_user_email(user) -> str:
+        return user_dal.get_user_email(user)
 
-def send_email_to_user(user_email: str):
-    print(user_email)
+    @staticmethod
+    def send_email_to_user(user_email: str):
+        print(user_email)
 
-try_send_email_to_users("Caimanes")
+EmailSender.try_send_email_to_users(1)
