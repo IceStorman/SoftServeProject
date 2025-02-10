a33f68214f4291932b9794f35c42d5a3caa209c3
Taras Protsiv <protsiv.taras2006@gmail.com>
2025-02-10 14:52:56 +0200

    fucking alembic
    (that shit only deleted unused column but didn't chanche the length of jti column)

--- database/models/token_blocklist.py
+++ database/models/token_blocklist.py
@@ -5,7 +5,7 @@ class TokenBlocklist(Base):
     __tablename__ = 'Token_Blocklist'
     id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("Users.user_id"))
-    jti = Column(String(36), nullable=False, index=True)
+    jti = Column(String, nullable=False, index=True)
     token_type = Column(String(10), nullable=False)
     revoked = Column(Boolean, default=False, nullable=False)
     expires_at = Column(DateTime, nullable=False)
