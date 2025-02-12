6d7edf80e3c3cf08335e06ea5caaa6e3013b063a
IceStorman <vladgamedeveloper@gmail.com>
2025-02-12 18:34:13 +0200

    Rewrited class in singleton

--- service/implementation/email_sender/user_subscription_manager.py
+++ service/implementation/email_sender/user_subscription_manager.py
@@ -1,5 +1,7 @@
 import os
 import base64
+from typing import Optional
+
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -16,42 +18,49 @@ from dotenv import load_dotenv
 
 from sqlalchemy import event
 
-SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
+class UserSubscriptionManagerMeta(type):
+    _instances = {}
 
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            _instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = _instance
+        return cls._instances[cls]
 
-class UserSubscriptionManager:
-    session = SessionLocal()
+class UserSubscriptionManager(metaclass=UserSubscriptionManagerMeta):
+    __SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
-    __current_dir = Path(__file__).resolve().parent if "__file__" in locals() else Path.cwd()
-    __envars = __current_dir / ".env"
-    load_dotenv(__envars)
+    def __init__(self):
+        self.__session = SessionLocal()
 
-    __user_subscription_dal = UserSubscriptionDAL(session)
+        current_dir = Path(__file__).resolve().parent if "__file__" in locals() else Path.cwd()
+        envars = current_dir / ".env"
+        load_dotenv(envars)
 
-    __sender_email = os.getenv("EMAIL")
-    __sender_password = os.getenv("PASSWORD")
+        self.__user_subscription_dal = UserSubscriptionDAL(self.__session)
 
-    @staticmethod
-    def add_subscribers_to_temp_table(team_index):
-        UserSubscriptionManager.__user_subscription_dal.add_subscribers_data(team_index)
+        self.__sender_email = os.getenv("EMAIL")
+        self.__sender_password = os.getenv("PASSWORD")
 
-    @staticmethod
-    def on_subscribers_inserted(mapper, connection, target):
-        UserSubscriptionManager.try_send_email_to_users()
+        event.listen(TempSubscribersData, "after_insert", self.__on_subscribers_inserted)
 
-    @staticmethod
-    def try_send_email_to_users():
-        subscribed_users = UserSubscriptionManager.__user_subscription_dal.get_subscribed_users_data_and_delete_rows()
+    def add_subscribers_to_temp_table(self, team_index):
+        self.__user_subscription_dal.add_subscribers_data(team_index)
+
+    def __on_subscribers_inserted(self, mapper, connection, target):
+        self.__try_send_email_to_users()
+
+    def __try_send_email_to_users(self):
+        subscribed_users = self.__user_subscription_dal.get_subscribed_users_data_and_delete_rows()
 
         email_subject = "News notification"
         email_body = "The news with the team that you subscribed in have just been released"
 
         for user in subscribed_users:
-            UserSubscriptionManager.__send_email_to_user(user.subscriber_emails, email_subject, email_body)
+            self.__send_email_to_user(user.subscriber_emails, email_subject, email_body)
 
-    @staticmethod
-    def __send_email_to_user(user_email: str, subject, body):
-        creds = UserSubscriptionManager.__authenticate_gmail()
+    def __send_email_to_user(self, user_email: str, subject, body):
+        creds = self.__authenticate_gmail()
         service = build("gmail", "v1", credentials=creds)
 
         message = MIMEText(body)
@@ -66,19 +75,18 @@ class UserSubscriptionManager:
 
         print(f"Email sent successfully! Message ID: {send_message['id']}")
 
-    @staticmethod
-    def __authenticate_gmail():
+    def __authenticate_gmail(self):
         creds = None
 
         if os.path.exists("token.json"):
-            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+            creds = Credentials.from_authorized_user_file("token.json", self.__SCOPES)
 
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
                 creds.refresh(Request())
             else:
                 credentials_path = r"C:\Users\LEGION\Documents\Softserve\SoftServeProject\credentials.json"
-                flow = InstalledAppFlow.from_client_secrets_file(credentials_path, SCOPES)
+                flow = InstalledAppFlow.from_client_secrets_file(credentials_path, self.__SCOPES)
                 creds = flow.run_local_server(port=0)
 
             with open("token.json", "w") as token:
@@ -86,6 +94,5 @@ class UserSubscriptionManager:
 
         return creds
 
-
-event.listen(TempSubscribersData, "after_insert", UserSubscriptionManager.on_subscribers_inserted)
-UserSubscriptionManager.add_subscribers_to_temp_table(7)
+manager = UserSubscriptionManager()
+manager.add_subscribers_to_temp_table(1)
