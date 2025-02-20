59b6cfcfc0729cabd39033d601fed64dcf3e7e38
Romlen2 <rlesak22@gmail.com>
2025-02-20 16:17:21 +0200

    small fix in base filter manager

--- service/api_logic/filter_manager/base_filter_manager.py
+++ service/api_logic/filter_manager/base_filter_manager.py
@@ -10,8 +10,5 @@ class BaseFilterManager:
         for filter_name, filter_value in filters_dict.items():
             if filter_name in cls.FILTERS and filter_value:
                 filter_method = getattr(cls, cls.FILTERS[filter_name])
-                if filter_name == "sport":
-                    query = filter_method(query, filter_value, session)
-                else:
-                    query = filter_method(query, filter_value)
+                query = filter_method(query, filter_value)
         return query
