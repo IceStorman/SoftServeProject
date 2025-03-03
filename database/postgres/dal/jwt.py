9062fbbf2671a1bc07d259c7fd3b5509834578ea
Taras4568 <protsiv.taras2006@gmail.com>
2025-03-03 18:02:16 +0200

    some changes in jwtDal and refreshDal

--- database/postgres/dal/jwt.py
+++ database/postgres/dal/jwt.py
@@ -4,13 +4,16 @@ from database.postgres.dto.jwt import jwtDTO
 from sqlalchemy.exc import SQLAlchemyError
 from typing import Optional, List
 from datetime import datetime
+from database.postgres.dal.user import UserDAL
 
 class jwtDAL:
     def __init__(self, db_session: Session):
         self.db_session = db_session
 
     def save_jwt(self, jwt_dto: jwtDTO) -> Optional[int]:
-        try:
+        user_dal = UserDAL(self.db_session)
+        user = user_dal.get_user_by_id(jwt_dto.user_id)
+        if user:
             jwt_entry = self.get_jwt_by_id(jwt_dto.id) if jwt_dto.id else None
             if jwt_entry:
                 jwt_entry.user_id = jwt_dto.user_id
@@ -26,11 +29,8 @@ class jwtDAL:
             self.db_session.commit()
             self.db_session.refresh(jwt_entry)
             return jwt_entry.id
-        except SQLAlchemyError as e:
-            self.db_session.rollback()
-            print(f"Error in save_jwt: {e}")
+        else:
             return None
-
 
     def save_jwts(self, jwt_dto_list: List[jwtDTO]):
         for jwt in jwt_dto_list:
@@ -50,35 +50,14 @@ class jwtDAL:
                 jwt_entry.revoked = True
                 jwt_entry.updated_at = datetime.utcnow()
                 self.db_session.commit()
-                return True
-            return False
+
         except SQLAlchemyError as e:
-            self.db_session.rollback()
-            print(f"Error in revoke_jwt: {e}")
-            return False
+            return f"Error in revoke_jwt: {e}"
+
 
     def revoke_refresh_token(self, jti: str) -> bool:
-        try:
-            jwt_entry = self.get_jwt_by_jti(jti)
-            if jwt_entry and jwt_entry.token_type == "refresh":
-                jwt_entry.revoked = True
-                jwt_entry.updated_at = datetime.utcnow()
-                self.db_session.commit()
-                return True
-            return False
-        except SQLAlchemyError as e:
-            self.db_session.rollback()
-            print(f"Error in revoke_refresh_token: {e}")
-            return False
-
-    def delete_expired_tokens(self) -> int:
-        try:
-            expired_count = self.db_session.query(TokenBlocklist).filter(
-                TokenBlocklist.expires_at < datetime.datetime.utcnow()
-            ).delete(synchronize_session=False)
+        jwt_entry = self.get_jwt_by_jti(jti)
+        if jwt_entry and jwt_entry.token_type == "refresh":
+            jwt_entry.revoked = True
+            jwt_entry.updated_at = datetime.utcnow()
             self.db_session.commit()
-            return expired_count
-        except SQLAlchemyError as e:
-            self.db_session.rollback()
-            print(f"Error while deleteng : {e}")
-            return 0
