c9a8a03a86d39f039f15e00b85e46b3d32336955
Andrew Kozovyi <andriy.kozovyi@gmail.com>
2025-03-26 17:49:12 +0200

    final changes

--- unit_tests/save_data_tests/test_save_players.py
+++ unit_tests/save_data_tests/test_save_players.py
@@ -1,19 +1,61 @@
 import pytest
 from unittest.mock import patch
 from database.postgres import save_api_data
+from database.session import SessionLocal
+
 
 class TestPlayersSavingData:
     @pytest.fixture(autouse=True)
     def setup(self):
         self.sport_name = "mma"
-        self.json_case_valid_parameters = {
+
+
+    @patch('database.postgres.dal.PlayerDal.save_players')
+    def test_parameters_with_team(self, mock_save_players):
+        json_case_valid_parameters = {
             "get": "players",
             'parameters': {
                 'team': 1488
             },
             "response": [{}]
         }
-        self.json_case_full_bad_info = {
+
+        save_api_data(json_case_valid_parameters, self.sport_name)
+        mock_save_players.assert_called_once()
+
+
+    @patch('database.postgres.dal.PlayerDal.save_players')
+    def test_full_info(self, mock_save_players):
+        json_case_full_info = {
+            "get": "players",
+            'parameters': {},
+            "response": [
+                {
+                    "id": 6,
+                    "name": "Neymar",
+                    "photo": "https://media.api-sports.io/football/players/276.png",
+                    "team": [
+                        {
+                            "id": 4,
+                            "name": "SBG Ireland"
+                        }
+                    ]
+                },
+                {
+                    "id": 3,
+                    "name": "John Doe",
+                    "photo": "https://example.com/john_doe.png",
+                }
+            ]
+        }
+
+        save_api_data(json_case_full_info, self.sport_name)
+        mock_save_players.assert_called_once()
+
+
+    @patch('database.postgres.dal.PlayerDal.save_players')
+    def test_full_bad_info(self, mock_save_players):
+        json_case_full_bad_info = {
             "get": "players",
             'parameters': {},
             "response": [
@@ -41,40 +83,6 @@ class TestPlayersSavingData:
                 }
             ]
         }
-        self.json_case_full_info = {
-            "get": "players",
-            'parameters': {},
-            "response": [
-                {
-                    "id": 6,
-                    "name": "Neymar",
-                    "photo": "https://media.api-sports.io/football/players/276.png",
-                    "team": [
-                        {
-                            "id": 4,
-                            "name": "SBG Ireland"
-                        }
-                    ]
-                },
-                {
-                    "id": 3,
-                    "name": "John Doe",
-                    "photo": "https://example.com/john_doe.png",
-                }
-            ]
-        }
 
-    @patch('database.postgres.dal.PlayerDal.save_players')
-    def test_parameters_dict_with_team(self, mock_save_players):
-        save_api_data(self.json_case_valid_parameters, self.sport_name)
-        mock_save_players.assert_called_once()
-
-    @patch('database.postgres.dal.PlayerDal.save_players')
-    def test_full_info(self, mock_save_players):
-        save_api_data(self.json_case_full_info, self.sport_name)
-        mock_save_players.assert_called_once()
-
-    @patch('database.postgres.dal.PlayerDal.save_players')
-    def test_full_bad_info(self, mock_save_players):
-        save_api_data(self.json_case_full_bad_info, self.sport_name)
+        save_api_data(json_case_full_bad_info, self.sport_name)
         mock_save_players.assert_called_once()
