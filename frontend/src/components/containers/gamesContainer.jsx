bd869aacf86bef229847425131066151ad0bb206
Andrew Kozovyi <andriy.kozovyi@gmail.com>
2025-03-29 12:36:53 +0200

    more garbage

--- frontend/src/components/containers/gamesContainer.jsx
+++ frontend/src/components/containers/gamesContainer.jsx
@@ -1,8 +1,4 @@
-import React, { useState } from 'react';
-import { SlArrowDown, SlArrowUp } from "react-icons/sl";
-import ReactPaginate from "react-paginate";
-import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
-import { TfiLayoutLineSolid } from "react-icons/tfi";
+import React from 'react';
 import StreamCard from "../cards/streamCard";
 
 const GamesContainer = ({
@@ -23,30 +19,6 @@ const GamesContainer = ({
 
             </div>
 
-            {!isCollapsed && (
-                <div className='games'>
-
-                    {children}
-
-                    <ReactPaginate
-                        breakLabel="..."
-                        nextLabel={currentPage === pageCount - 1 ? <TfiLayoutLineSolid className="line" /> : <SlArrowRight className="arrow" />}
-                        previousLabel={currentPage === 0 ? <TfiLayoutLineSolid className="line" /> : <SlArrowLeft className="arrow" />}
-                        onPageChange={onPageChange}
-                        pageRangeDisplayed={3}
-                        marginPagesDisplayed={1}
-                        pageCount={pageCount}
-                        forcePage={currentPage}
-                        renderOnZeroPageCount={null}
-                        activeClassName="activePaginationPane"
-                        containerClassName="pagination-games"
-                        pageLinkClassName="page-num"
-                        previousLinkClassName="page-prev"
-                        nextLinkClassName="page-next"
-                        activeLinkClassName="page-active"
-                        key={paginationKey}
-                    />
-                </div>)}
         </div>
     );
 };
