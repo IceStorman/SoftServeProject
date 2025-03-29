458164568aec68ffd4214984f8a2ffac4355961b
Andrew Kozovyi <andriy.kozovyi@gmail.com>
2025-03-29 19:39:48 +0200

    google tests

--- unit_tests/auth_tests/test_user_service.py
+++ unit_tests/auth_tests/test_user_service.py
@@ -1,13 +1,16 @@
-from unittest.mock import AsyncMock, MagicMock
+from unittest.mock import AsyncMock, MagicMock, patch
 import bcrypt
 import pytest
 from dependency_injector import providers, containers
 from flask import Flask
+from oauthlib.oauth2.rfc6749.clients.web_application import WebApplicationClient
 from database.models import User
 from dto.api_input import InputUserLogInDTO
 from exept.exeptions import UserAlreadyExistError, UserDoesNotExistError, IncorrectUserDataError, \
     IncorrectLogInStrategyError
+from service.api_logic.auth_strategy import AuthManager
 from service.api_logic.user_logic import UserService
+from werkzeug.local import LocalProxy
 
 
 class TestContainer(containers.DeclarativeContainer):
@@ -35,9 +38,15 @@ class TestUserService:
         self.app = Flask(__name__)
         self.app_context = self.app.app_context()
         self.app.config["SECRET_KEY"] = "test_secret"
+        self.app.config["GOOGLE_CLIENT_ID"] = "test_client_id"
+        self.app.config["TOKEN_URL"] = "test_token_url"
+        self.app.config["GOOGLE_CLIENT_SECRET"] = "test_client_secret_google"
+        self.app.config["REDIRECT_URI"] = "test_redirect_uri"
+        self.app.config["USER_INFO_URL"] = "test_test_user_info_url"
         self.app_context.push()
         self.container = TestContainer()
         self.user_service = self.container.user_service()
+        self.auth_manager = AuthManager(self.user_service)
 
         log_in_info = {
             "email_or_username": "andriy.kozovyi@gmail.com",
@@ -138,3 +147,38 @@ class TestUserService:
         with self.app.app_context():
             with pytest.raises(IncorrectLogInStrategyError, match=".* is not a log in method in this app"):
                 await self.user_service.log_in(dto)
+
+    @pytest.mark.asyncio
+    @patch('requests.post')
+    @patch('requests.get')
+    @patch('oauthlib.oauth2.rfc6749.clients.WebApplicationClient.prepare_token_request')
+    async def test_authenticate(self, mock_prepare_token_request, mock_requests_get, mock_requests_post):
+        with self.app.test_request_context():
+            mock_prepare_token_request.return_value = ('test_token_url', {}, '')
+
+            mock_requests_post.return_value.ok = True
+            mock_requests_post.return_value.text = '{"access_token": "fake_access_token"}'
+
+            mock_requests_get.return_value.ok = True
+            mock_requests_get.return_value.json.return_value = {"email": "andriy.kozovyi@gmail.com"}
+
+            self.user_service.get_user_by_email_or_username = MagicMock()
+            self.user_service.create_user = MagicMock()
+            self.user_service._refresh_dal.get_valid_tokens_by_user = MagicMock(
+                return_value=("mock_access_token", "mock_refresh_token"))
+            self.user_service.create_tokens = AsyncMock(return_value=("mock_access_token", "mock_refresh_token"))
+
+            self.user_service.get_generate_auth_token = AsyncMock(return_value="generated_token")
+
+            google = {
+                "auth_provider": "google"
+            }
+            dto = InputUserLogInDTO().load(google)
+
+            result_log_in = await self.user_service.log_in(dto)
+
+            self.user_service.get_user_by_email_or_username = MagicMock(return_value=None)
+            result_sign_up = await self.user_service.log_in(dto)
+
+            assert result_log_in.user_id is not None
+            assert result_sign_up.user_id is not None
