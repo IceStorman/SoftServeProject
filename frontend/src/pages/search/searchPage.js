6fe5022bfed148f701dba4afe3842ba8cdbeb116
Romlen2 <rlesak22@gmail.com>
2025-03-29 14:34:29 +0200

    add logic for league

--- frontend/src/pages/search/searchPage.js
+++ frontend/src/pages/search/searchPage.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import FiltersRenderer from "../../components/filters/filterRender";
-import GamesContainer from "../../components/containers/gamesContainer";
-import StreamCard from "../../components/cards/streamCard";
 import useTranslations from "../../translationsContext";
 import axios from "axios";
 import apiEndpoints from "../../apiEndpoints";
@@ -11,149 +9,145 @@ import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 import ReactPaginate from "react-paginate";
 import useBurgerMenu from "../../customHooks/useBurgerMenu";
 import globalVariables from "../../globalVariables";
-import {FaFilter, FaTimes} from "react-icons/fa";
+import { FaFilter, FaTimes } from "react-icons/fa";
 import useBurgerMenuState from "../../customHooks/useBurgerMenuState";
+import SearchBlock from "../../components/containers/searchBlock";
+import StreamCard from "../../components/cards/streamCard";
+import LeagueCard from "../../components/cards/leagueCard";
+import TeamCard from "../../components/cards/teamCard";
 
-function SearchPage(){
+function SearchPage() {
     const { t } = useTranslations();
     const [selectedModel, setSelectedModel] = useState("streams");
     const [filters, setFilters] = useState([]);
-    const [currentStreams, setCurrentStreams] = useState([]);
+    const [currentItems, setCurrentItems] = useState([]);
     const [pageCount, setPageCount] = useState(0);
     const [currentPage, setCurrentPage] = useState(0);
-    const [inputValue, setInputValue] = useState('');
-    const [prevInputValue, setPrevInputValue] = useState('');
-    const burgerMenu = useBurgerMenu(`${globalVariables.windowSizeForBurger.streams}`);
-    const initialIcon = <FaFilter size={28} />;
-    const closeIcon = <FaTimes size={28} color="black" />;
+    const burgerMenu = useBurgerMenu(globalVariables.windowSizeForBurger.streams);
+    const [sportId, setSportId] = useState(null);
+    const [leagueId, setLeagueId] = useState(null);
+    const [inputValue, setInputValue] = useState(""); 
+    const [prevInputValue, setPrevInputValue] = useState("");
 
     const { menuIsOpen, menuIcon, handleOpenMenu, handleCloseMenu } = useBurgerMenuState({
         menuSelector: ".filters-container",
         buttonSelector: ".menu-btn",
-        initialIcon: initialIcon,
-        closeIcon: closeIcon,
+        initialIcon: <FaFilter size={28} />, 
+        closeIcon: <FaTimes size={28} color="black" />,
     });
 
-    const getStreams = async (page = 0) => {
+    const componentMap = (type, item) => {
+        const componentsCard = {
+            leagues: <LeagueCard
+                    leagueName={item.name}
+                    img={item.logo}
+                    id={item.id}
+                    sportId={sportId}
+                    />,
+        }
+        console.log(componentsCard[type]);
+        return componentsCard[type] || {};
+    };
+
+    const fetchData = async (model, page = 0) => {
         setPrevInputValue(inputValue);
-
+    
+        let filtersData = [...filters];
+    
+        if (sportId) {
+            filtersData.push({ filter_name: "sport_id", filter_value: sportId });
+        }
+        if (leagueId) {
+            filtersData.push({ filter_name: "league_id", filter_value: leagueId });
+        }
+    
         try {
+            const endpointMap = {
+                streams: apiEndpoints.stream.getStreamsSearch,
+                leagues: apiEndpoints.sports.getLeagueSearch,
+                teams: apiEndpoints.teams.getTeamsAll, 
+            };
+    
+            if (!endpointMap[model]) {
+                throw new Error(`Unknown model: ${model}`);
+            }
+    
             const response = await axios.post(
-                `${apiEndpoints.url}${apiEndpoints.stream.getStreamsSearch}`,
+                `${apiEndpoints.url}${endpointMap[model]}`,
                 {
-                    pagination: {
-                        page: page + 1,
-                        per_page: 10,
-                    },
-                    filters: filters
+                    pagination: { page: page + 1, per_page: 10 },
+                    filters: filtersData,
                 },
-                {
-                    headers: { 'Content-Type': 'application/json' },
-                }
+                { headers: { "Content-Type": "application/json" } }
             );
-
-            setCurrentStreams(response.data.items);
+    
+            setCurrentItems(response.data.items || []);
             setPageCount(Math.ceil(response.data.count / 10));
+            console.log(response.data.items)
         } catch (error) {
             setPageCount(0);
-            toast.error(`Troubles With Leagues Loading: ${error}`);
+            toast.error(`Error loading ${model}: ${error.message}`);
         }
-    };
+    };    
 
     useEffect(() => {
-        getStreams(0);
-    }, []);
-
-    const handlePageClick = (event) => {
-        const selectedPage = event.selected;
-        setCurrentPage(selectedPage);
-        getStreams(selectedPage);
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    };
-
-    const handleFiltersChange = (newFilters) => {
-        setFilters(newFilters);
-    };
-
-    const handleModelChange = (newModel) => {
-        setSelectedModel(newModel)
-    }
-
-    const handleApplyFilters = () => {
-        handleCloseMenu()
-        setCurrentPage(0);
-        getStreams(0);
-    };
-
-    const filtersBlock = (
-        <div className={`filters-container ${menuIsOpen ? "show" : ""}`}>
-            <FiltersRenderer model={selectedModel} onFilterChange={handleFiltersChange} />
-            <button onClick={handleApplyFilters}>{t("apply_filters")}</button>
-        </div>
-    )
-
-    const models = ["streams", "leagues", "teams"];
-
-    const modelSelectionMenu = (
-        <div className="model-selection">
-            {models.map((model) => (
-                <button
-                    key={model}  
-                    className={selectedModel === model ? "active" : ""}
-                    onClick={() => handleModelChange(model)}
-                >
-                    {model.charAt(0).toUpperCase() + model.slice(1)}  {}
-                </button>
-            ))}
-        </div>
-    );
-
-
+        fetchData(selectedModel, 0);
+    }, [selectedModel, filters]);
 
     return (
         <div className="streams-page">
-
-            {modelSelectionMenu}
-            
-            {!burgerMenu ? filtersBlock : null}
-
-            <div className="content">
-
-                {burgerMenu  && (
-                    <div className="filter-wrapper">
-                        <button className={"menu-btn"} onClick={handleOpenMenu}>{menuIcon}</button>
-                        {menuIsOpen && filtersBlock }
-                    </div>
-                )}
-
-                {
-                    currentStreams ?
-                        <GamesContainer
-                            streams={currentStreams}
-                        /> : null
-                }
-
-                {pageCount > 1 && (
-                    <ReactPaginate
-                        breakLabel="..."
-                        nextLabel={currentPage === pageCount - 1 ? <TfiLayoutLineSolid className="line"/> :
-                            <SlArrowRight className="arrow"/>}
-                        previousLabel={currentPage === 0 ? <TfiLayoutLineSolid className="line"/> :
-                            <SlArrowLeft className="arrow"/>}
-                        onPageChange={handlePageClick}
-                        pageRangeDisplayed={3}
-                        marginPagesDisplayed={1}
-                        pageCount={pageCount}
-                        renderOnZeroPageCount={null}
-                        activeClassName="activePaginationPane"
-                        containerClassName="pagination-games"
-                        pageLinkClassName="page-num"
-                        previousLinkClassName="page-prev"
-                        nextLinkClassName="page-next"
-                        activeLinkClassName="page-active"
-                    />
-                )}
+            <div className="model-selection">
+                {["streams", "leagues", "teams"].map((model) => (
+                    <button
+                        key={model}
+                        className={selectedModel === model ? "active" : ""}
+                        onClick={() => setSelectedModel(model)}
+                    >
+                        {model.charAt(0).toUpperCase() + model.slice(1)}
+                    </button>
+                ))}
             </div>
+
+            {!burgerMenu && (
+                <div className={`filters-container ${menuIsOpen ? "show" : ""}`}>
+                    <FiltersRenderer model={selectedModel} onFilterChange={setFilters} />
+                    <button onClick={() => fetchData(selectedModel, 0)}>{t("apply_filters")}</button>
+                </div>
+            )}
+
+            {burgerMenu && (
+                <div className="filter-wrapper">
+                    <button className={"menu-btn"} onClick={handleOpenMenu}>{menuIcon}</button>
+                    {menuIsOpen && (
+                        <div className="filters-container show">
+                            <FiltersRenderer model={selectedModel} onFilterChange={setFilters} />
+                            <button onClick={() => fetchData(selectedModel, 0)}>{t("apply_filters")}</button>
+                        </div>
+                    )}
+                </div>
+            )}
+
+            <SearchBlock
+                gridSize={{ columns: 3, rows: 2, cardSize: { width: 4000, height: 400 } }}
+                postsPerPage={10}
+                pageCount={pageCount}
+                currentPage={currentPage}
+                onPageChange={(event) => {
+                    setCurrentPage(event.selected);
+                    fetchData(selectedModel, event.selected);
+                }}
+                paginationKey={selectedModel}
+                count={currentItems.length}
+            
+                children={currentItems.length > 0 ? (
+                    currentItems.map((item) => {
+                       return componentMap(selectedModel, item);
+                    })
+                ) : (
+                    <div>{t("no_results_found")}</div>
+                )}
+            >
+            </SearchBlock>
         </div>
     );
 }
