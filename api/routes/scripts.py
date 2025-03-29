bcb61fb2cc75788b7ad59defdd4c2a44cb31bdfc
Iarynovskyi <dimayar20061@gmail.com>
2025-03-29 09:03:14 +0200

    delete unused def

--- api/routes/scripts.py
+++ api/routes/scripts.py
@@ -8,7 +8,6 @@ from logger.logger import Logger
 logger = Logger("logger", "all.log")
 
 
-
 def get_cache_key():
     sport_type = request.view_args.get('sport_type')
     logger.debug(f"Cache key for sport type: {sport_type}")
@@ -21,15 +20,6 @@ def post_cache_key():
     hashed_key = hashlib.md5(json_string.encode('utf-8')).hexdigest()
     logger.debug(f"Generated cache key: {hashed_key} for method {request.method} on path {request.path}")
     return f"{request.method}:{request.path}:{hashed_key}"
-
-
-def check_positive_param(param_name):
-    param_value = request.args.get(param_name, type=int)
-    if param_value is None or param_value <= 0:
-        logger.warning(f"Invalid parameter value for {param_name}: {param_value}")
-        return False
-    logger.debug(f"Valid parameter value for {param_name}: {param_value}")
-    return True
 
 
 def get_recommendation_key():
