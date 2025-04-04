3e630570c5091783e84ec28cd3c4003f1ef8918d
Taras Protsiv <protsiv.taras2006@gmail.com>
2025-04-04 22:49:44 +0300

    check

--- frontend/src/components/containers/newsShowcase.jsx
+++ frontend/src/components/containers/newsShowcase.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 import { TfiLayoutLineSolid } from "react-icons/tfi";
 import NoItems from "../NoItems";
@@ -10,16 +10,44 @@ import '../../scss/pagination.scss'
 function NewsShowcase({ newsData }) {
     const [loading, setLoading] = useState(false);
     const [currentPage, setCurrentPage] = useState(1);
+    const [dimensions, setDimensions] = useState({ height: 37.5, width: 31.25 });
+
+    useEffect(() => {
+        const updateSize = () => {
+            const width = window.innerWidth;
+            let newDimensions;
+    
+            if (width <= 480) {
+                newDimensions = { height: 28, width: 22 };
+            } else if (width <= 768) {
+                newDimensions = { height: 33, width: 27 };
+            } else if (width <= 1024) {
+                newDimensions = { height: 36, width: 30 };
+            } else {
+                newDimensions = { height: 37.5, width: 31.25 };
+            }
+    
+            if (
+                newDimensions.height !== dimensions.height ||
+                newDimensions.width !== dimensions.width
+            ) {
+                setDimensions(newDimensions);
+            }
+        };
+    
+        window.addEventListener("resize", updateSize);
+        updateSize();
+    
+        return () => window.removeEventListener("resize", updateSize);
+    }, [dimensions]);
+    
 
     const postsPerPage = 1;
     const pageCount = Math.ceil(newsData.length / postsPerPage);
-    const element_height = 37.5;
-    const element_width = 31.25;
 
     const handlePageChange = ({ selected }) => {
         setCurrentPage(selected + 1);
     };
-
 
     return (
         <div className="newsbox">
@@ -33,9 +61,10 @@ function NewsShowcase({ newsData }) {
                     id={newsData[currentPage - 1]?.blob_id}
                     article={newsData[currentPage - 1]?.data}
                     isFoil={true}
+                    height={dimensions.height}
+                    width={dimensions.width}
                     className="custom-size"
                 />
-            
             ) : !loading ? (
                 <NoItems key={1} text={"No latest news were found"} />
             ) : null}
